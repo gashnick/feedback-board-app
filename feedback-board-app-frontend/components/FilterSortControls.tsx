3d320fe648dba4ff7a24fa40281a9ab689890dc9
Gashumba Nicolas <gashnicolas@gmail.com>
2025-05-31 18:48:45 +0200

    Change project title in layout.tsx file

--- feedback-board-app-frontend/components/FilterSortControls.tsx
+++ feedback-board-app-frontend/components/FilterSortControls.tsx
@@ -25,7 +25,7 @@ const FilterSortControls: React.FC<FilterSortControlsProps> = ({
   return (
     <div className="mb-6 p-4 bg-neutral-light rounded-lg shadow-sm flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0 sm:space-x-4">
       {/* Category Filter */}
-      <div>
+      <div className="flex items-center space-x-2 mb-2 sm:mb-0">
         <label
           htmlFor="category-filter"
           className="block text-sm font-medium text-neutral-dark mb-1 sm:mb-0 sm:mr-2"
@@ -38,11 +38,13 @@ const FilterSortControls: React.FC<FilterSortControlsProps> = ({
           onChange={(e: ChangeEvent<HTMLSelectElement>) =>
             onCategoryChange(e.target.value)
           }
-          className="input-field py-2 text-sm w-full sm:w-auto text-black"
+          className="input-field bg-white py-2 text-sm w-full sm:w-auto "
         >
-          <option value="">All Categories</option>
+          <option value="" className="text-black bg-white">
+            All Categories
+          </option>
           {categories.map((cat) => (
-            <option key={cat} value={cat} className="capitalize">
+            <option key={cat} value={cat} className="capitalize text-black">
               {cat.charAt(0).toUpperCase() + cat.slice(1)}
             </option>
           ))}
